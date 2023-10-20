2ae9a00adfcfa107787fea52690c54e739c2947d
frogtickler <115919964+frogtickler@users.noreply.github.com>
2023-10-20 19:13:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from PIL import PcfFontFile
 from barcode import EAN13
 from barcode.writer import ImageWriter
 
+st.title('Fraktsedel')
+st.header('Skapa en fraktsedel enkelt och gratis') 
 
-googelver = "google-site-verification=QDSSXnIdodTtZaa3EZn7ySDp8eE69zEHiXkXZFoccdw"
 
 
 st.set_page_config(page_title="Skapa en enkel fraktsedel", page_icon=":flag-sv:", layout="wide")
